009389b5b5bbfdee051e26066e20fe1553d58724
e-l-l <pratham1986@gmail.com>
2026-07-15 23:36:18 +0530

    Initial Commit

---
+++ stellar/stellarApp.swift
@@ -0,0 +1,17 @@
+//
+//  stellarApp.swift
+//  stellar
+//
+//  Created by Prathmesh Ghatol on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct stellarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
